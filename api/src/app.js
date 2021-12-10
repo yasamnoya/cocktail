1bfe62868c96f7ec6c1d2061b1954d763bd0ca27
J.N. Yiunn <yasamnono@gmail.com>
2021-12-10 14:59:18 +0800

    init dirs

---
+++ api/src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const router = require('./routes');
+
+const app = express();
+
+app.use(morgan('common'));
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(router);
+
+module.exports = app;
