8bab5ab9600038efc8025f649a9d996b8a73cda0
J.N. Yiunn <yasamnono@gmail.com>
2021-12-11 15:02:33 +0800

    add bootstrap

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 import './registerServiceWorker';
 import router from './router';
 
