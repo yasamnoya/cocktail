17b154f72b0baabcba2ed42d6d2c97380fef0360
J.N. Yiunn <yasamnono@gmail.com>
2021-12-11 17:11:33 +0800

    add list recipes page

--- client/src/router/recipes.js
+++ client/src/router/recipes.js
@@ -1,5 +1,10 @@
 const routes = [
   {
+    path: '/recipes',
+    name: 'ListRecipes',
+    component: () => import('../views/recipe/ListRecipes.vue'),
+  },
+  {
     path: '/recipes/new',
     name: 'CreateRecipe',
     component: () => import('../views/recipe/CreateRecipe.vue'),
---
+++ client/src/views/recipe/ListRecipes.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="card">
+    <ul class="list-group list-group-flush">
+      <li v-for="recipe in recipes" :key="recipe.id" class="list-group-item">
+        <div class="card-title fs-3">{{ recipe.title }}</div>
+        <div class="card-text">{{ recipe.instruction }}</div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data: () => ({
+    recipes: [],
+  }),
+  async created() {
+    try {
+      const res = await axios.get('/recipes');
+      this.recipes = res.data;
+    } catch (e) {
+      console.warn(e);
+    }
+  },
+};
+</script>
